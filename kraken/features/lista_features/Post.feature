a07cb2bcacb21b92517cf478b7279d199c529381
milindr4123 <e.foreros@uniandes.edu.co>
2023-11-13 22:04:39 -0500

    add features to post

---
+++ kraken/features/lista_features/Post.feature
@@ -0,0 +1,63 @@
+Feature: Creación Post y visualizacion
+
+@user1 @web
+Scenario: As a user, I create and view a post.
+  Given I navigate to post "http://localhost:2368/ghost/#/signin"
+  When  I am logged "<USERNAME1>" and "<PASSWORD1>"
+  When  I click in Post link 
+  When  I click in New Post link "//a[@href='#/editor/post/']"
+  When  I click the Post settings button
+  When  I enter Post URL "New Post in GHOST"
+  When  I click the Post settings button
+  When  I click in Post link 
+  When  I click in Editor button
+  When  I click in Post link 
+
+
+@user2 @web
+Scenario: As a user, I create and view a post.
+  Given I navigate to post "http://localhost:2368/ghost/#/signin"
+  When  I am logged "<USERNAME1>" and "<PASSWORD1>"
+  When  I click in Post link 
+  When  I click in New Post link "//a[@href='#/editor/post/']"
+  When  I click the Post settings button
+  When  I enter Post URL "New Post in GHOST"
+  When  I click the Post settings button
+  When  I click in Post link 
+  When  I click in Editor button  
+  When  I Edit a Post "Es editado"
+  When  I click in Post link 
+
+
+@user3 @web
+Scenario: As a user, I create and view a post.
+  Given I navigate to post "http://localhost:2368/ghost/#/signin"
+  When  I am logged "<USERNAME1>" and "<PASSWORD1>"
+  When  I click in Post link 
+  When  I click in New Post link "//a[@href='#/editor/post/']"
+  When  I click the Post settings button
+  When  I enter Post URL "New Post in GHOST"
+  When  I click the Post settings button
+  When  I click in Post link 
+  When  I click in Editor button  
+  When  I Edit a Post "Es editado"
+  When  I Publish Post
+  When  I click in Post link 
+
+
+
+@user4 @web
+Scenario: As a user, I create and view a post.
+  Given I navigate to post "http://localhost:2368/ghost/#/signin"
+  When  I am logged "<USERNAME1>" and "<PASSWORD1>"
+  When  I click in Post link 
+  When  I click in New Post link "//a[@href='#/editor/post/']"
+  When  I click the Post settings button
+  When  I enter Post URL "New Post in GHOST"
+  When  I click the Post settings button
+  When  I click in Post link 
+  When  I click in Editor button  
+  When  I Edit a Post "Es editado"
+  When  I click the Post settings button
+  When  I remove Post
+  When  I click in Post link 
--- kraken/features/web/step_definitions/step.js
+++ kraken/features/web/step_definitions/step.js
@@ -207,3 +207,96 @@ Then('I click the first tag', async function() {
     await tagElement.click();
 })
 
+const { Given, When, Then } = require('@cucumber/cucumber'); 
+
+Given('I am logged {kraken-string} and {kraken-string}', async function(email, password) {
+    let identificationElement = await this.driver.$('#identification');
+    await identificationElement.setValue(email);
+
+    let passwordElement = await this.driver.$('#password');
+    await passwordElement.setValue(password);
+ 
+    let element = await this.driver.$('#ember5');
+    return await element.click();
+})
+
+Given('I navigate to post {string}', async function (postURL) {
+    await this.driver.url(postURL);
+});
+ 
+
+When('I click in Post link', async function () {
+    let element = await this.driver.$('a[href="#/posts/"]');
+    await element.waitForExist({ timeout: 10000 });
+    await element.waitForDisplayed({ timeout: 10000 });    
+    await element.waitForClickable({ timeout: 10000 });
+    await element.click();
+});
+
+When('I click in New Post link {kraken-string}', async function (enlace) {
+    let element = await this.driver.$(enlace);
+    await element.click();
+});
+
+When('I click the Post settings button', async function () {
+    let element  = await this.driver.$('button.settings-menu-toggle');
+    element.click();
+});
+
+When('I enter Post URL {kraken-string}', async function (url) {
+    let element = await this.driver.$('#url');
+    await element.setValue(url);
+});
+
+
+When('I click in Editor button', async function () {
+    let element  = await this.driver.$('span.gh-post-list-cta.edit');
+    await element.waitForExist({ timeout: 3000 });
+    await element.waitForDisplayed({ timeout: 3000 });    
+    await element.waitForClickable({ timeout: 3000 });    
+    await element.click();
+});
+
+
+//Escenario 2
+When('I Edit a Post {kraken-string}', async function (message) {
+    let element = await this.driver.$('textarea.gh-editor-title.ember-text-area.gh-input.ember-view');
+    await element.click();
+    await element.setValue(message);
+});
+
+When('I Publish Post', async function () {
+    let element = await this.driver.$('button.gh-btn.gh-btn-editor.darkgrey.gh-publish-trigger');
+    await element.click();
+    let element2 = await this.driver.$('button.gh-btn.gh-btn-black.gh-btn-large');
+    await element2.waitForExist({ timeout: 3000 });
+    await element2.waitForDisplayed({ timeout: 3000 });    
+    await element2.waitForClickable({ timeout: 3000 }); 
+    await element2.click();
+});
+
+
+When('I Publish Post', async function () {
+    let element = await this.driver.$('button.gh-btn.gh-btn-editor.darkgrey.gh-publish-trigger');
+    await element.click();
+    let element2 = await this.driver.$('button.gh-btn.gh-btn-black.gh-btn-large');
+    await element2.waitForExist({ timeout: 3000 });
+    await element2.waitForDisplayed({ timeout: 3000 });    
+    await element2.waitForClickable({ timeout: 3000 }); 
+    await element2.click();
+});
+
+
+When('I remove Post', async function () {
+    let element = await this.driver.$('settings-menu-delete-button');
+    await element.click();
+    await element2.waitForExist({ timeout: 3000 });
+    await element2.waitForDisplayed({ timeout: 3000 });    
+    await element2.waitForClickable({ timeout: 3000 }); 
+    await element2.click();
+});
+
+
+
+
+
