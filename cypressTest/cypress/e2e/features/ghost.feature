63998d90167fca739e01ff71fc38febb0fb3b401
Jeniffer Corredor <j.corredore@uniandes.edu.co>
2023-11-17 23:03:44 -0500

    Adición escenarios de prueba

--- cypressTest/cypress/e2e/features/ghost.feature
+++ cypressTest/cypress/e2e/features/ghost.feature
@@ -15,8 +15,95 @@ Feature: Creación, Edición, Publicación, Despublicación y Eliminación de un
         Then  The settings menu should be opened
         When  I click again on the page settings button
         When  I click in Preview button
-        Then  I should be redirected to the page
         When  I click in Editor button
         Then  I should be redirected to the page
         When  I click in Pages link again
         Then  I should be redirected to the pages
+
+    Scenario: As a user, I create, edit and view a page.
+        Given I navigate to page Ghost
+        When  I enter email
+        When  I enter password
+        When  I click in Sign in button
+        Then  I should be redirected to the dashboard
+        When  I click in Pages link
+        Then  I should be redirected to the pages
+        When  I click the "New page" link
+        Then  I should be redirected to the new page
+        When  I enter title "New Page in Ghost - title 2"
+        When  I click the Page settings button
+        Then  The settings menu should be opened
+        When  I click again on the page settings button
+        When  I click in Preview button
+        When  I click in Editor button
+        Then  I should be redirected to the page
+        When  I enter New title " Edit"
+        When  I click in Preview button
+        When  I click in Editor button
+        Then  I should be redirected to the page
+        When  I click in Pages link again
+        Then  I should be redirected to the pages
+
+    Scenario: As a user, I create, publish and view a page.
+        Given I navigate to page Ghost
+        When  I enter email
+        And   I enter password
+        And   I click in Sign in button
+        Then  I should be redirected to the dashboard
+        When  I click in Pages link
+        Then  I should be redirected to the pages
+        When  I click the "New page" link
+        Then  I should be redirected to the new page
+        When  I enter title "New Page in Ghost - title 3"
+        And   I click the Page settings button
+        Then  The settings menu should be opened
+        When  I click again on the page settings button
+        And   I click in Preview button
+        And   I click in Editor button 
+        And   I click in Publish button
+        And   I click in Continue, final review button
+        And   I click in Publish page, right now button    
+        And   I click in Published Page
+        Then  I should be redirected to the Published page
+
+    Scenario: As a user, I create, edit, delete and view a page.
+        Given I navigate to page Ghost
+        When  I enter email
+        And   I enter password
+        And   I click in Sign in button
+        Then  I should be redirected to the dashboard
+        When  I click in Pages link
+        Then  I should be redirected to the pages
+        When  I click the "New page" link
+        Then  I should be redirected to the new page
+        When  I enter title "New Page in Ghost - title 4"
+        And   I click the Page settings button
+        Then  The settings menu should be opened
+        When  I click in the Delete page button
+        And   I click in the Delete button
+        Then  I should be redirected
+
+    Scenario: As a user, I create, publish and unpublish a page.
+        Given I navigate to page Ghost
+        When  I enter email
+        And   I enter password
+        And   I click in Sign in button
+        Then  I should be redirected to the dashboard
+        When  I click in Pages link
+        Then  I should be redirected to the pages
+        When  I click the "New page" link
+        Then  I should be redirected to the new page
+        When  I enter title "New Page in Ghost - title 5"
+        And   I click the Page settings button
+        Then  The settings menu should be opened
+        When  I click again on the page settings button
+        And   I click in Preview button
+        And   I click in Editor button 
+        And   I click in Publish button
+        And   I click in Continue, final review button
+        And   I click in Publish page, right now button
+        And   I click in Back to Editor link
+        And   I click in Unpublish button
+        And   I click in Unpublish and revert to private draft button
+        And   I click in Pages link again
+        Then  I should be redirected
--- cypressTest/cypress/e2e/step_definition/ghost.js
+++ cypressTest/cypress/e2e/step_definition/ghost.js
@@ -2,84 +2,173 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Given('I navigate to page Ghost', () => {
   cy.visit("http://localhost:2368/ghost/#/signin");
-  
+  cy.screenshot('SignInPage');
 });
 
 When("I enter email", () => {
   cy.get("#identification").type("j.corredore@uniandes.edu.co");
+  cy.screenshot('EnterYourEmail');
   cy.wait(1000);
 });
 
 When("I enter password", () => {
   cy.get("#password").type("01234567890");
+  cy.screenshot('EnterYourPassword');
   cy.wait(1000);
 });
 
 When("I click in Sign in button", () => {
   cy.get("#ember5").click();
+  cy.screenshot('ClickSignInButton');
   cy.wait(2000);
 });
 
 Then("I should be redirected to the dashboard", () => {
   cy.url().should("eq", "http://localhost:2368/ghost/#/dashboard");
+  cy.screenshot('DashboardPage');
   cy.wait(5000);
 });
 
 When("I click in Pages link", () => {
   cy.get("a[href='#/pages/']").click();
+  cy.screenshot('PageLink');
   cy.wait(3000);
 });
 
 Then("I should be redirected to the pages", () => {
   cy.url().should("eq", "http://localhost:2368/ghost/#/pages");
+  cy.screenshot('PagesPage1');
   cy.wait(5000);
 });
 
 When('I click the {string} link', (linkText) => {
   cy.get('section.view-actions').contains(linkText).click();
+  cy.screenshot('NewPagesButton');
   cy.wait(5000);
 });
 
 Then("I should be redirected to the new page", () => {
   cy.url().should("eq", "http://localhost:2368/ghost/#/editor/page");
+  cy.screenshot('NewPage1');
   cy.wait(2000);
 });
 
 When("I enter title {string}", (title) => {
   cy.get("textarea.gh-editor-title").type(title);
+  cy.screenshot('EnterTitlePage');
   cy.wait(1000);
 });
 
 When("I click the Page settings button", () => {
   cy.get("button.settings-menu-toggle").click();
+  cy.screenshot('PageSettingsMenu');
   cy.wait(2000);
 });
 
 Then('The settings menu should be opened', () => {
   cy.get('div.settings-menu-container').should('be.visible');
+  cy.screenshot('PageSettingsMenuVisible');
+  cy.wait(2000);
 });
 
 When("I click again on the page settings button", () => {
   cy.get("button.settings-menu-toggle").click();
+  cy.screenshot('PageSettingsMenuAgain');
   cy.wait(1000);
 });
 
 When("I click in Preview button", () => {
   cy.get('button[data-test-button="publish-preview"]').click();
+  cy.screenshot('PreviewButton');
   cy.wait(5000);
 });
 
 When("I click in Editor button", () => {
   cy.get('.left').find('button.gh-editor-back-button').click()
+  cy.screenshot('EditorButton');
   cy.wait(5000);
 });
 
 Then("I should be redirected to the page", () => {
   cy.url().should("include", "http://localhost:2368/ghost/#/editor/page/");
+  cy.screenshot('NewPageRedirected');
   cy.wait(2000);
 });
 
 When("I click in Pages link again", () => {
   cy.get("a[href='#/pages/']").click();
+  cy.screenshot('PageLink2');
   cy.wait(2000);
+});
+
+When("I enter New title {string}", (title) => {
+  cy.get("textarea.gh-editor-title").type(title);
+  cy.screenshot('EnterTitlePage');
+  cy.wait(1000);
+});
+
+When("I click in Publish button", () => {
+  cy.get('section button[data-test-button="publish-flow"]').click();
+  cy.screenshot('PublishButton');
+  cy.wait(5000);
+});
+
+When("I click in Continue, final review button", () => {
+  cy.get('button[data-test-button="continue"]').click()
+  cy.screenshot('ContinuePublish');
+  cy.wait(5000);
+});
+
+When("I click in Publish page, right now button", () => {
+  cy.get('button[data-test-button="confirm-publish"]').click();
+  cy.screenshot('ConfirmPublish');
+  cy.wait(3000);
+});
+
+When("I click in Published Page", () => {
+  cy.get('div.gh-post-bookmark-image').click();
+  cy.screenshot('PagePublished');
+  cy.wait(3000);
+});
+
+Then("I should be redirected to the Published page", () => {
+  cy.url().should("include", "http://localhost:2368/ghost/#/editor/page/");
+  cy.screenshot('NewPageRedirected');
+  cy.wait(2000);
+});
+
+When("I click in the Delete page button", () => {
+  cy.get("button.gh-btn-outline").click();
+  cy.screenshot('DeletePageButton');
+  cy.wait(3000);
+});
+
+When("I click in the Delete button", () => {
+  cy.get("button.gh-btn-red").click();
+  cy.screenshot('DeleteButton');
+  cy.wait(3000);
+});
+
+Then("I should be redirected", () => {
+  cy.url().should("eq", "http://localhost:2368/ghost/#/pages");
+  cy.screenshot('Pages');
+  cy.wait(2000);
+});
+
+When("I click in Back to Editor link", () => {
+  cy.get('button[data-test-button="back-to-editor"]').click();
+  cy.screenshot('BackToEditor');
+  cy.wait(3000);
+});
+
+When("I click in Unpublish button", () => {
+  cy.get('section button[data-test-button="update-flow"]').click();
+  cy.screenshot('UnpublishButton');
+  cy.wait(3000);
+});
+
+When("I click in Unpublish and revert to private draft button", () => {
+  cy.get('button[data-test-button="revert-to-draft"]').click();
+  cy.screenshot('Unpublish');
+  cy.wait(3000);
 });
