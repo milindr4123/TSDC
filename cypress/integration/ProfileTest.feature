fcc40f8e8dbf1b02c519646c08bd7b73c1056a45
milindr4123 <e.foreros@uniandes.edu.co>
2023-11-25 14:15:22 -0500

    test profiles

--- cypress/integration/ProfileTest.feature
+++ cypress/integration/ProfileTest.feature
@@ -41,3 +41,50 @@ Feature: Update user profile using Ghost web app
         And I entered new password twice
         And I click on Change password button
         Then  The toast error "Your password is incorrect."
+
+    Scenario: Change password in profile with password under 10 characters
+        Given I am logged
+        When I click on avatar icon
+        And I click on Your profile
+        And I click on Change password button
+        And I entered old password
+        And I entered new short password
+        And I click on Change password button
+        Then  The error message "Password must be at least 10 characters long."
+
+    Scenario: I Fill form without full name
+        Given I am logged
+        When I click on avatar icon
+        And I click on Your profile
+        And I fill form
+        Then  The error message "Please enter a name"
+
+    Scenario: I fill form with name too long
+        Given I am logged
+        When I click on avatar icon
+        And I click on Your profile
+        And I fill form with name too long
+        Then  The error message "Name is too long"
+
+
+    Scenario: I fill form with invalid email
+        Given I am logged
+        When I click on avatar icon
+        And I click on Your profile
+        And I fill form with invalid email
+        Then  The error message "Please enter a valid email address"
+
+    Scenario: I fill form with location too long
+        Given I am logged
+        When I click on avatar icon
+        And I click on Your profile
+        And I fill form with location too long
+        Then  The error message "Location is too long"
+
+    Scenario: I fill form with website invalid URL
+        Given I am logged
+        When I click on avatar icon
+        And I click on Your profile
+        And I fill form with website invalid URL
+        Then  The error message "Please enter a valid URL"
+
--- cypress/integration/ProfileTest/ProfileTest.cy.js
+++ cypress/integration/ProfileTest/ProfileTest.cy.js
@@ -1,4 +1,6 @@
 import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps"
+import {faker} from '@faker-js/faker'
+
 
 Cypress.on('uncaught:exception', (err, runnable) => {
     if (err.message.includes('TransitionAborted') || err.message.includes('TaskCancelation') || err.message.includes('NiceModal.Provider')) {
@@ -126,6 +128,21 @@ And("I entered new password differents", () => {
     cy.screenshot(datetime + '-ProfileGhostV5/TypeNewPassword');
 });
 
+And("I entered new short password", () => {
+    cy.fixture('credentials').then((profile) => {
+        cy.get('input.peer[type="password"]').then(inputs => {
+            var inputText = inputs.get(0);
+            inputText = inputs.get(1);
+            cy.wrap(inputText).type(profile.shortPassword, {force: true});
+            cy.wait(1000)
+            inputText = inputs.get(2);
+            cy.wrap(inputText).type(profile.shortPassword, {force: true});
+        });
+    });
+    cy.wait(1000)
+    cy.screenshot(datetime + '-ProfileGhostV5/TypeNewPassword');
+});
+
 
 And("I back to Dashboard view", () => {
     cy.get('[data-testid="exit-settings"]').click();
@@ -153,6 +170,86 @@ And("I entered with email and new password", () => {
     cy.wait(1000)
     cy.screenshot(datetime + '-ProfileGhostV5/SignInPage');
 });
+
+And("I fill form", () => {
+    cy.fixture('profile').then((profile) => {
+        cy.get('input.peer[type="text"]').then(inputs => {
+            var inputText = inputs.get(0);
+            inputText = inputs.get(0);
+            cy.wrap(inputText).clear();
+            cy.wait(1000)
+            inputText = inputs.get(2);
+            cy.wrap(inputText).clear().type(profile.location, {force: true});
+        });
+    });
+    cy.wait(1000)
+    cy.screenshot(datetime + '-ProfileGhostV5/TypeNewPassword');
+});
+
+
+And("I fill form with name too long", () => {
+    cy.fixture('profile').then((profile) => {
+        cy.get('input.peer[type="text"]').then(inputs => {
+            var inputText = inputs.get(0);
+            inputText = inputs.get(0);
+            cy.wrap(inputText).clear().type(faker.lorem.sentence(30));
+            cy.wait(1000)
+            inputText = inputs.get(2);
+            cy.wrap(inputText).clear().type(profile.location, {force: true});
+        });
+    });
+    cy.wait(1000)
+    cy.screenshot(datetime + '-ProfileGhostV5/TypeNewPassword');
+});
+
+
+
+And("I fill form with location too long", () => {
+    cy.fixture('profile').then((profile) => {
+        cy.get('input.peer[type="text"]').then(inputs => {
+            var inputText = inputs.get(0);
+            inputText = inputs.get(3);
+            cy.wrap(inputText).clear().type(faker.lorem.sentence(40));
+            cy.wait(1000)
+            inputText = inputs.get(2);
+            cy.wrap(inputText).clear().type(profile.location, {force: true});
+        });
+    });
+    cy.wait(1000)
+    cy.screenshot(datetime + '-ProfileGhostV5/TypeNewPassword');
+});
+
+And("I fill form with invalid email", () => {
+    cy.fixture('profile').then((profile) => {
+        cy.get('input.peer[type="text"]').then(inputs => {
+            var inputText = inputs.get(0);
+            inputText = inputs.get(1);
+            cy.wrap(inputText).clear().type(faker.lorem.sentence(10));
+            cy.wait(1000)
+            inputText = inputs.get(2);
+            cy.wrap(inputText).clear().type(profile.location, {force: true});
+        });
+    });
+    cy.wait(1000)
+    cy.screenshot(datetime + '-ProfileGhostV5/TypeNewPassword');
+});
+
+
+And("I fill form with website invalid URL", () => {
+    cy.fixture('profile').then((profile) => {
+        cy.get('input.peer[type="text"]').then(inputs => {
+            var inputText = inputs.get(0);
+            inputText = inputs.get(4);
+            cy.wrap(inputText).clear().type(profile.webSiteInvalid);
+            cy.wait(1000)
+            inputText = inputs.get(2);
+            cy.wrap(inputText).clear().type(profile.location, {force: true});
+        });
+    });
+    cy.wait(1000)
+    cy.screenshot(datetime + '-ProfileGhostV5/TypeNewPassword');
+});
+
 
 Then("I am logged", () => {
     cy.url().then((url) => {
