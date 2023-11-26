836daf6185ed50644e7030746a31c12f6de4afa3
milindr4123 <e.foreros@uniandes.edu.co>
2023-11-26 14:09:59 -0500

    options test

--- cypress/integration/OptionTest.feature
+++ cypress/integration/OptionTest.feature
@@ -5,8 +5,38 @@ Feature: options test in Ghost
         And I entered email and password
         And I click on login submit button
 
-   
+
+    Scenario: I fill twitter profile form with random method
+        Given I am logged
+        When I click on settings button
+        And I click on integrations button
+        And I click on slack integration button
+        And I fill webhook url random method
+        Then  The error message "The URL must be in a format like https://hooks.slack.com/services/<your personal key>"
+
+
+    
+    Scenario Outline: I fill twitter profile form with dynamic data successful
+        Given I am logged
+        When I click on settings button
+        And I click on integrations button
+        And I click on slack integration button
+        And I fill webhook url "<webhook>"
+        And I click on Save & close button
+        And I click on slack integration button
+        Then I compare values "<webhook>"
+         Examples:
+        | webhook   |
+        | https://www.deepl.com/es/translator |
+        
+  
     Scenario Outline: I fill twitter profile form with dynamic data 
         Given I am logged
         When I click on settings button
-        Then  The error message
+        And I click on integrations button
+        And I click on slack integration button
+        And I fill webhook url "<webhook>"
+        Then  The error message "<error>"
+        Examples:
+        | webhook   | error |
+        | sinurl | The URL must be in a format like https://hooks.slack.com/services/<your personal key> |
--- cypress/integration/OptionTest/OptionTest.js
+++ cypress/integration/OptionTest/OptionTest.js
@@ -18,7 +18,7 @@ Given("I navigate to the Website", () => {
         portVersion = port.v5
         cy.visit(`http://localhost:${portVersion}/ghost/`);
     });
-    cy.screenshot(datetime + '-ProfileGhostV5/SignInPage');
+    cy.screenshot(datetime + '-OptionsGhostV5/SignInPage');
 });
 
 And("I entered email and password", () => {
@@ -26,31 +26,91 @@ And("I entered email and password", () => {
         cy.get('input[name="identification"]').type(credentials.email, {force: true})
         cy.get('input[name="password"]').type(credentials.password, {force: true})
     });
-    cy.screenshot(datetime + '-ProfileGhostV5/EmailAndPassword');
+    cy.screenshot(datetime + '-OptionsGhostV5/EmailAndPassword');
 });
 
 And("I click on login submit button", () => {
     cy.get('button[type="submit"]').click()
-    cy.screenshot(datetime + '-ProfileGhostV5/LoginButton');
+    cy.screenshot(datetime + '-OptionsGhostV5/LoginButton');
 });
 
 Given("I am logged", () => {
     cy.url().then((url) => {
         cy.url().should('eq', `http://localhost:${portVersion}/ghost/#/dashboard`);
     })
-    cy.screenshot(datetime + '-ProfileGhostV5/DashboardPage');
+    cy.screenshot(datetime + '-OptionsGhostV5/DashboardPage');
 });
 
 
 When("I click on settings button", () => {
     cy.get('a[href="#/settings/"]').click();
     cy.wait(1000)
-    cy.screenshot(datetime + "-IntegrationsGhostV5/SettingsButton");
+    cy.screenshot(datetime + "-OptionsGhostV5/SettingsButton");
 
 });
 
-Then('The error message', () => {
-    cy.get('p.main-error').invoke('text').should('contains', "");
-    cy.screenshot(datetime + '-PageGhostV5/PageErrorForgotPassword');
+
+
+When("I click on integrations button", () => {
+    cy.get('a[id="integrations"]').click();
+    cy.wait(1000)
+    cy.screenshot(datetime + "-OptionsGhostV5/IntegrationsButton");
+
+});
+
+
+When("I click on slack integration button", () => {
+    cy.get('div[class="group/list-item flex items-center justify-between hover:bg-gradient-to-r hover:from-white hover:to-grey-50 dark:hover:from-black dark:hover:to-grey-950 border-b border-grey-100 last-of-type:border-b-transparent hover:border-grey-200 dark:border-grey-900 dark:hover:border-grey-800"]').then(div => {
+        var slack = div.get(1);
+        cy.wrap(slack).click();
+    });
+    cy.wait(1000)
+    cy.screenshot(datetime + "-OptionsGhostV5/ZapierIntegrationsButton");
+
+});
+
+When("I fill webhook url {string}", (webhook) => {
+    cy.get('div[class="relative order-2 flex w-full items-center mt-1.5"]').find('input').then(inputs => {
+        var inputText = inputs.get(0);
+        cy.wrap(inputText).clear().type(webhook, {force: true});
+        var inputText = inputs.get(1);
+        cy.wrap(inputText).click();
+        cy.wait(1000)
+        cy.screenshot(datetime + "-OptionsGhostV5/FillSlackUrl");
+    });
+});
+
+
+When("I click on Save & close button", () => {
+    cy.contains('span', 'Save & close').click();
+    cy.wait(1000)
+    cy.screenshot(datetime + '-OptionsGhostV5/SaveAndCloseButton');
+});
+
+Then("I compare values {string}", (value) => {
+    cy.get('div[class="relative order-2 flex w-full items-center mt-1.5"]').find('input').then(inputs => {
+        var inputText = inputs.get(0);
+        cy.wrap(inputText).should('have.value', value)
+        cy.wait(1000)
+        cy.screenshot(datetime + "-OptionsGhostV5/CompareText");
+    });
+});
+
+
+
+When("I fill webhook url random method", () => {
+    cy.get('div[class="relative order-2 flex w-full items-center mt-1.5"]').find('input').then(inputs => {
+        var inputText = inputs.get(0);
+        cy.wrap(inputText).clear().type(faker.lorem.sentence(2), {force: true});
+        var inputText = inputs.get(1);
+        cy.wrap(inputText).click();
+        cy.wait(1000)
+        cy.screenshot(datetime + "-OptionsGhostV5/FillSlackUrl");
+    });
+});
+
+Then('The error message {string}', (error) => {
+    cy.get('span[class="mt-1 inline-block text-xs text-red dark:text-red-500 order-3"]').invoke('text').should('contains', error);
+    cy.screenshot(datetime + '-OptionsV5/PageErrorFillSlackUrl');
     cy.wait(5000);
   });
